e1ea2a066acd4ad93c8ac744643ce9014ed7fd2a
Manuel Vila <mvila@3base.com>
2016-04-19 15:01:34 +0900

    Update iOS and Android native code and remove Windows support

--- src/ios/SQLitePlugin.h
+++ src/ios/SQLitePlugin.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012-2015 Chris Brody
+ * Copyright (c) 2012-2016: Christopher J. Brody (aka Chris Brody)
  * Copyright (C) 2011 Davide Bertola
  *
  * This library is available under the terms of the MIT License (2008).
@@ -30,26 +30,21 @@ typedef int WebSQLError;
 @property (nonatomic, copy) NSMutableDictionary *openDBs;
 @property (nonatomic, copy) NSMutableDictionary *appDBPaths;
 
-// Open / Close
+// Self-test
+-(void) echoStringValue: (CDVInvokedUrlCommand*)command;
+
+// Open / Close / Delete
 -(void) open: (CDVInvokedUrlCommand*)command;
 -(void) close: (CDVInvokedUrlCommand*)command;
 -(void) delete: (CDVInvokedUrlCommand*)command;
 
+-(void) openNow: (CDVInvokedUrlCommand*)command;
+-(void) closeNow: (CDVInvokedUrlCommand*)command;
+-(void) deleteNow: (CDVInvokedUrlCommand*)command;
+
 // Batch processing interface
 -(void) backgroundExecuteSqlBatch: (CDVInvokedUrlCommand*)command;
--(void) executeSqlBatch: (CDVInvokedUrlCommand*)command;
 
-// Single requests interface
--(void) backgroundExecuteSql:(CDVInvokedUrlCommand*)command;
--(void) executeSql:(CDVInvokedUrlCommand*)command;
-
-// Perform the SQL request
--(CDVPluginResult*) executeSqlWithDict: (NSMutableDictionary*)dict andArgs: (NSMutableDictionary*)dbargs;
-
--(id) getDBPath:(NSString *)dbFile at:(NSString *)atkey;
-
-+(NSDictionary *)captureSQLiteErrorFromDb:(struct sqlite3 *)db;
-
-+(int)mapSQLiteErrorCode:(int)code;
+-(void) executeSqlBatchNow: (CDVInvokedUrlCommand*)command;
 
 @end /* vim: set expandtab : */
